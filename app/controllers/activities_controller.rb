1d2e9d2ccdd30d63330a39e027a800523a30d659
victor fulconis <victor.fulconis@gmail.com>
2016-02-15 18:09:54 +0100

    Partial work on activity crude, carefull, mistakes on the form, untested (need User model), date not suppressed (need all branches merged

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,48 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = current_user.activities.new(activity_params)
+    if @activity.save
+      redirect_to activity_path(@activity.id), method :get
+    else
+      render new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity.id), method :get
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @activity.delete
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:name, :starting_time, :end_time, :price, :pictogram_link, :description)
+  end
 end
