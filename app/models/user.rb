33e9dd697ed664bf4f2c29c5857f0a56e4d641b2
Edgar Chaput <chaput.edgar@gmail.com>
2016-02-16 10:17:44 +0100

    user model created with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # has_many :activities, dependent: :destroy
-  # has_many :bookings, dependent: :destroy
+  has_many :activities, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
   validates :surname, presence: true
