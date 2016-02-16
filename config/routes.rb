d5b911501a25cf971410697a09fc78f20d8578ca
Louis-Clement Schiltz <me@louis-clement.com>
2016-02-16 11:37:10 +0100

    added the logic inside the bookings controller + added the routes + modified the view for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/home'
-
   devise_for :users
   root to: "pages#home"
+  resources :bookings, only: [:new, :create, :index, :delete]
+  resources :activities
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
