2131475cf72f8d1a2558b1829706e44324f0ee9e
victor fulconis <victor.fulconis@gmail.com>
2016-02-17 17:24:54 +0100

    improved seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,50 +6,178 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-activitiesrange = %w( Cuisine Informatique Conversation Echec)
+# AUTOMATIC UGLY VERSION
 
-10.times do
-  user = User.new(
-    name: Faker::Name.first_name,
-    surname: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    address: "16 villa Gaudelet, 75011",
-    password: "00000000",
-    birthdate: "24/01/1960",
-    description: Faker::Lorem.paragraph(2, false)
-  )
-  user.save!
-  2.times do
-    Activity.create!(
-      name: activitiesrange[rand(0..3)],
-      starting_time: Faker::Date.forward(2).to_s,
-      end_time: (Faker::Date.forward(2) + 4).to_s,
-      price: Faker::Commerce.price.to_i,
-      pictogram_link: "#",
-      description: Faker::Lorem.paragraph(2, false),
-      user_id: user.id
-    )
-  end
-end
+# activitiesrange = %w( Cuisine Informatique Conversation Echec)
 
-
-# 1.times do
+# 10.times do
 #   user = User.new(
-#     name: "Papy",
-#     surname: "Marcel",
-#     email: "papy.marcel@jaimelesjeunes.fr",
+#     name: Faker::Name.first_name,
+#     surname: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     address: "16 villa Gaudelet, 75011",
 #     password: "00000000",
-#     birthdate: "24/01/1930",
-#     description: "Jovial et accueillant, je pratique ma passion avec assiduité!"
+#     birthdate: "24/01/1960",
+#     description: Faker::Lorem.paragraph(2, false)
 #   )
 #   user.save!
-#   Activity.create!(
-#     name: "Branlette",
-#     starting_time: "20/02/2016",
-#     end_time: "24/02/2016",
-#     price: 12,
-#     pictogram_link: "#",
-#     description: "Sportif et détendant, un plaisir de tous les jours",
-#     user_id: user.id
-#   )
+#   2.times do
+#     Activity.create!(
+#       name: activitiesrange[rand(0..3)],
+#       starting_time: Faker::Date.forward(2).to_s,
+#       end_time: (Faker::Date.forward(2) + 4).to_s,
+#       price: Faker::Commerce.price.to_i,
+#       pictogram_link: "#",
+#       description: Faker::Lorem.paragraph(2, false),
+#       user_id: user.id
+#     )
+#   end
 # end
+
+# NICE CUSTOM VERSION
+
+
+@user = User.new(
+  email: "e@gmail.com",
+  password: "aaaaaaaa",
+  name: "Marcel",
+  surname: "Georges",
+  description: "J'aime manger des nouilles",
+  photo_link: "",
+  birthdate: "1930-01-12",
+  address: "Villa Gaudelet, Paris"
+)
+@user.save!
+Activity.create!(
+  name: "Cuisine",
+  starting_time: "18/02/2016",
+  end_time: "27/02/2016",
+  price: 23,
+  pictogram_link: "",
+  description: "J'ai besoin d'aide pour cuire beaucoup de nouilles!",
+  user_id: @user.id
+)
+Activity.create!(
+  name: "Conversation",
+  starting_time: "20/02/2016",
+  end_time: "27/02/2016",
+  price: 12,
+  pictogram_link: "",
+  description: "Je vais bien te faire chier!",
+  user_id: @user.id
+)
+
+@user = User.new(
+  email: "b@gmail.com",
+  password: "aaaaaaaa",
+  name: "Basile",
+  surname: "Toutoublie",
+  description: "Love meeting new friends :)",
+  photo_link: "",
+  birthdate: "1933-06-15",
+  address: "Paris"
+)
+@user.save!
+Activity.create!(
+  name: "Conversation",
+  starting_time: "18/02/2016",
+  end_time: "27/02/2016",
+  price: 23,
+  pictogram_link: "",
+  description: "Je t'aurai oublié demain!",
+  user_id: @user.id
+)
+
+@user = User.new(
+  email: "f@gmail.com",
+  password: "aaaaaaaa",
+  name: "Lucette",
+  surname: "Jeanine",
+  description: "J'aime faire de longues siestes  entourés de mes 15 chats",
+  photo_link: "",
+  birthdate: "1945-06-56",
+  address: "Rue du fond du pré"
+)
+@user.save!
+Activity.create!(
+  name: "Conversation",
+  starting_time: "19/02/2016",
+  end_time: "29/02/2016",
+  price: 59,
+  pictogram_link: "",
+  description: "Venez parler de chats avec moi jusqu'à 18h",
+  user_id: @user.id
+)
+
+@user = User.new(
+  email: "c@gmail.com",
+  password: "aaaaaaaa",
+  name: "Jeannine",
+  surname: "Super",
+  description: "J'adore faire la cuisine",
+  photo_link: "",
+  birthdate: "1938-03-14",
+  address: "Maison de retraite les vielles sont cool"
+)
+@user.save!
+Activity.create!(
+  name: "Cuisine",
+  starting_time: "20/02/2016",
+  end_time: "20/02/2016",
+  price: 5,
+  pictogram_link: "",
+  description: "Participation de 5€ pour un déjeuner du feu de dieu",
+  user_id: @user.id
+)
+
+@user = User.new(
+  email: "a@gmail.com",
+  password: "aaaaaaaa",
+  name: "Albert",
+  surname: "Einstein",
+  description: "Let's do some maths together !",
+  photo_link: "",
+  birthdate: "1922-08-29",
+  address: "Krakow"
+)
+@user.save!
+Activity.create!(
+  name: "Informatique",
+  starting_time: "18/02/2016",
+  end_time: "29/02/2016",
+  price: 30,
+  pictogram_link: "",
+  description: "I don't understand anything about computers. Help ...",
+  user_id: @user.id
+)
+Activity.create!(
+  name: "Echec",
+  starting_time: "20/02/2016",
+  end_time: "02/03/2016",
+  price: 50,
+  pictogram_link: "",
+  description: "Hardcore chess player",
+  user_id: @user.id
+)
+
+@user = User.new(
+  email: "d@gmail.com",
+  password: "aaaaaaaa",
+  name: "Jordy",
+  surname: "L'ancien",
+  description: "J'ai un prénom de jeune!",
+  photo_link: nil,
+  birthdate: "1940-03-26",
+  address: "Loin d'ici"
+)
+@user.save!
+Activity.create!(
+  name: "Informatique",
+  starting_time: "19/02/2016",
+  end_time: "27/02/2016",
+  price: 12,
+  pictogram_link: "",
+  description: "Je ne sais pas me servir d'un PC!",
+  user_id: @user.id
+)
+
