3c4311cab9f1092e02e205edfbd976b0c55e593c
victor fulconis <victor.fulconis@gmail.com>
2016-02-17 11:20:21 +0100

    Added basic search

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,15 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, inclusion: {in: %w( Cuisine Informatique Conversation Echec)}
   validates :starting_time, presence: true
   validates :end_time, presence: true
   validates :price, presence: true
+
+  def self.search(query)
+    where(name: query[:name])
+  end
 end
+
+
+# && where(:starting_time >= query[:starting_time]) && where(:end_time <= query[:end_time])
